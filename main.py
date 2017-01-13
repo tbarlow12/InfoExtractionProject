1fe60bd3129cab26a2d2dc4489039c703d10b7c7
tbarlow12 <tanner.barlow12@gmail.com>
2017-01-13 15:20:59 -0700

    answering summary and age questions

--- main.py
+++ main.py
@@ -2,33 +2,59 @@
 #set PYTHONIOENCODING=utf-8
 #run these commands in windows console to get utf-8 encoding
 import wikipedia
+import unicodedata
 import fileinput
 import datetime
-from datetime import timedelta
+#from datetime import timedelta
+from dateutil.relativedelta import relativedelta #pip install python-dateutil==1.5
 import sys
 import re #https://docs.python.org/2/howto/regex.html
 
-summaryRegex = 'Who is (.*)\?'
-ageRegex = 'How old is (.*)\?'
+summaryRegex = 'who is (.*)\?'
+ageRegex = 'how old is (.*)\?'
+ageRegex2 = 'what is the age of (.*)\?'
+questionList = [summaryRegex,ageRegex,ageRegex2];
 
-questionList = [summaryRegex,ageRegex];
+def answerType(questionType,person):
+    return {
+        1: person.summary,
+        2: person.age,
+        3: person.age
+    }[questionType]
 
 sectionRegex = '===? (.*) ===?\\n((?:[^(?:===? .* ===)]|\\s|\.|\(|\)|\:)*)'
-dateRegex = '([a-zA-Z]{3,}) (\d){1,2}(?:th|nd|rd)?, (\d{4})'
+dateRegex = '([a-zA-Z]{3,}) (\d{1,2})(?:th|nd|rd)?, (\d{4})'
+yearRegex = '(\\d+)'
+monthRegex = '(January|February|March|April|June|July|August|September|October|November|December)'
+dayRegex = '(\\d{1,2})(?:th|nd|rd)?'
+
+def difYears(start,finish):
+    if finish is None:
+        finish = datetime.datetime.now()
+    return relativedelta(finish,start)
+
+def make_date(year,month,day):
+    d = datetime.datetime(year,month,day,0,0,0)
+    return d
 
 class Person(object):
-    name = ""
-    birth = datetime.MINYEAR
-    death = datetime.MINYEAR
 
-    def __init__(self,name,birth,death):
+    def __init__(self,name,birth,death,summary):
         self.name = name
         self.birth = birth
         self.death = death
+        self.age = difYears(birth,death).years
+        self.summary = summary
 
-def make_person(name,birth,death):
-    person = Person(name,birth,death)
+def make_person(name,birth,death,summary):
+    person = Person(name,birth,death,summary)
     return person
+
+def learnPerson(subject,summary,sections):
+    birth = getBirthday(summary)
+    death = getDeathday(summary)
+    p = make_person(subject,birth,death,summary)
+    return p
 
 #Returns list of tuples where first is section title and second is section content
 def getSections(content):
@@ -39,37 +65,50 @@ def getSections(content):
 def getMonthNum(m):
     return {
         'january': 1,
+        'jan': 1,
         'february': 2,
+        'feb': 2,
         'march': 3,
+        'mar': 3,
         'april': 4,
+        'apr': 4,
         'may': 5,
         'june': 6,
+        'jun': 6,
         'july': 7,
+        'jul': 7,
         'august': 8,
+        'aug': 8,
         'september': 9,
+        'sep': 9,
         'october': 10,
+        'oct': 10,
         'november': 11,
+        'nov': 11,
         'december': 12,
+        'dec': 12
     }[m]
 
-def getDate(dateString):
-    p = re.compile(dateRegex)
-    m = p.match(dateString)
-    monthString = m.group(1)
-    day = m.group(2)
-    year = m.group(3)
-    month = getMonthNum(monthString.lower())
-    print month
-    print day
-    print year
+def getDate(tup):
+    monthString = tup[0].lower()
+    month = getMonthNum(monthString)
+    day = int(tup[1])
+    year = int(tup[2])
+    return make_date(year,month,day)
 
 def getBirthday(summary):
-    print 'date'
-    getDate(summary)
-    return datetime.date(2015,3,12)
+    p = re.compile('born ' + dateRegex,re.I)
+    l = re.findall(p,summary)
+    if(len(l) > 0):
+        return getDate(l[0])
+    return None
 
 def getDeathday(summary):
-    return datetime.date(2015,3,12)
+    p = re.compile('died ' + dateRegex,re.I)
+    l = re.findall(p,summary)
+    if(len(l) > 0):
+        return getDate(l[len(l)-1])
+    return None
 
 def getSubjectFromRegex(regex,question):
     subject = ''
@@ -90,8 +129,6 @@ def getSubject(question):
         i += 1
     return result
 
-
-
 def getSections(content):
     p = re.compile(sectionRegex)
     matches = re.findall(p,content)
@@ -100,22 +137,25 @@ def getSections(content):
 def getAnswer(question):
     questionResult = getSubject(question)
     questionType = questionResult[0]
-    print questionType
     subject = questionResult[1]
     if (len(subject) > 0):
         page = wikipedia.page(subject)
-        summary = page.summary
+        summary = page.summary.encode('utf-8')
+        #unicodedata.normalize('NFKD',summary).encode('ascii','ignore')
         content = page.content.encode('utf-8')
         sections = getSections(content)
-        return page.title
+        p = learnPerson(subject,summary,sections)
+        return answerType(questionType,p)
     return 'I couldn\'t find an answer to that question'
 
 def main():
+
     while True:
         question = sys.stdin.readline()
         if(len(question) == 0):
             return
-        print 'Question: ' + question
+        print question
+        question = question.lower()
         print getAnswer(question)
 
 
